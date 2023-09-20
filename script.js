2a3d194bdca4822e917534b548300a00ce8b64bb
mborzechowsk <m.borzechowski@gmail.com>
2023-09-21 00:08:34 +0200

    canvas plus stałe

---
+++ script.js
@@ -0,0 +1,54 @@
+const canvas = document.querySelector("#gameCanvas");
+const ctx = canvas.getContext("2d");
+
+const CANVAS_HEIGHT = canvas.height;
+const CANVAS_WIDTH = canvas.width;
+
+const BOARD_Y = 50; // y dla punktacji
+const BOARD_P1_X = 300;
+const BOARD_P2_X = 500;
+
+const PADDLE_WIDTH = 20;
+const PADDLE_HEIGHT = 100;
+const PADDLE_P1_X = 10;
+const PADDLE_P2_X = 770;
+const PADDLE_START_Y = (CANVAS_HEIGHT - PADDLE_HEIGHT) / 2
+
+const BALL_R = 15; // promień piłeczki
+const BALL_START_X = CANVAS_WIDTH / 2;
+const BALL_START_Y = CANVAS_HEIGHT / 2;
+const BALL_START_DX = 4.5; // początkowa prędkość lotu piłeczki na współrzędnej x
+const BALL_START_DY = 1.5 // początkowa prędkość lotu piłeczki na współrzeędnej y
+
+
+ctx.font = "30px Arial"
+
+function drawPaddle(x, y) {
+    ctx.fillRect(x, y, 20, 100)
+}
+
+function drawText(text, x, y) {
+    ctx.fillText(text, x, y);
+}
+
+function drawCircle(x, y, r) {
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fill();
+}
+
+
+function clearCanvas() {
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+}
+
+drawPaddle(770, 100);
+drawPaddle(10, 300);
+
+drawText('3', 300, 50);
+drawText('6', 500, 50);
+
+drawCircle(400,250,10);
+
+
